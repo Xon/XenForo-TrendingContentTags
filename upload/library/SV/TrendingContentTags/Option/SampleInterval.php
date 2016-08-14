8316cbf91f5e395bfa4b19d26218780a1cf7f8ce
Xon <tazipper1@hotmail.com>
2016-08-14 15:05:38 +0800

    Bugfix error on initial install

--- upload/library/SV/TrendingContentTags/Option/SampleInterval.php
+++ upload/library/SV/TrendingContentTags/Option/SampleInterval.php
@@ -5,7 +5,11 @@ class SV_TrendingContentTags_Option_SampleInterval
     public static function verifyOption(&$option, XenForo_DataWriter $dw, $fieldName)
     {
         $tagModel = XenForo_Model::create('XenForo_Model_Tag');
-        $tagModel->PersistTrendingTags(true);
+        // on initial install, our extensions to the XenForo_Model_Tag may not have been implemented
+        if (is_callable(array($tagModel, 'PersistTrendingTags'))
+        {
+            $tagModel->PersistTrendingTags(true);
+        }
         return true;
     }
 }
